a6fc5b249d061a06271ba1ca23e522f03d29c2a7
Alekesi <91783474+Alekesi@users.noreply.github.com>
2021-10-05 20:59:10 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static int calculateFactorial(int n){
+                int result = 1;
+                for (int i = 1; i <=n; i ++){
+                    result = result*i;
+                }
+                return result;
+            }
+
+            public static void main(String[] args){
+                System.out.println(calculateFactorial(7));
+            }
+        }
+
+
